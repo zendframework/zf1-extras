2a0cd50cfce7c616fcd2d35da48389c47a4a28b4
beberlei <beberlei@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-06-11 08:19:28 +0000

    Bugfix ZF-6048, ZF-5841, ZF-5810 - Slider default values and handles did not work with jQuery UI 1.7 due to API changes from 1.5 to 1.7. Setting setUiVersion() lets jQuery Slider Helper pick the correct javascript to generate now.
    git-svn-id: http://framework.zend.com/svn/framework/extras/trunk@15990 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/ZendX/JQuery/View/Helper/Slider.php
+++ library/ZendX/JQuery/View/Helper/Slider.php
@@ -54,23 +54,13 @@ class ZendX_JQuery_View_Helper_Slider extends ZendX_JQuery_View_Helper_UiWidget
 
         $jqh = ZendX_JQuery_View_Helper_JQuery::getJQueryHandler();
 
-        $handleCount = 1;
-        if(isset($params['handles']) && is_array($params['handles'])) {
-            $handleCount = min(count($params['handles']), 1);
-        }
-        if(!isset($params['handles'][0]['start'])) {
-            if(is_numeric($value)) {
-                $params['startValue'] = $value;
-            } elseif(!isset($params['startValue'])) {
-                $params['startValue'] = 0;
-            }
-        }
+        $params = $this->initializeStartingValues($value, $params);
+        $handleCount = $this->getHandleCount($params);
 
         // Build the Change/Update functionality of the Slider via javascript, updating hidden fields. aswell as hidden fields
         $hidden = "";
-        $sliderUpdateFnName = "zfjSliderUpdate".preg_replace('/([^a-zA-Z0-9]+)/', '', $attribs['id']);
         if(!isset($params['change'])) {
-            $sliderUpdateFn = sprintf('function %s(e, ui) {'.PHP_EOL, $sliderUpdateFnName);
+            $sliderUpdateFn = 'function(e, ui) {'.PHP_EOL;
             for($i = 0; $i < $handleCount; $i++) {
                 // Js Func
                 if($i === 0) {
@@ -78,25 +68,21 @@ class ZendX_JQuery_View_Helper_Slider extends ZendX_JQuery_View_Helper_UiWidget
                 } else {
                     $sliderHiddenId = $attribs['id']."-".$i;
                 }
-                $sliderUpdateFn .= sprintf('    %s("#%s").attr("value", %s("#%s-slider").slider("value", %d));'.PHP_EOL,
-                    $jqh, $sliderHiddenId, $jqh, $attribs['id'], $i);
+                $sliderUpdateFn .= $this->getChangeCallback($jqh, $sliderHiddenId, $attribs['id'], $i);
 
                 // Hidden Fields
-                $startValue = (isset($params['handles'][$i]['start']))?$params['handles'][$i]['start']:$params['startValue'];
+                $startValue = $this->getHandleValue($i, $params);
                 $hiddenAttribs = array('type' => 'hidden', 'id' => $sliderHiddenId, 'name' => $sliderHiddenId, 'value' => $startValue);
                 $hidden .= '<input' . $this->_htmlAttribs($hiddenAttribs) . $this->getClosingBracket(). PHP_EOL;
             }
             $sliderUpdateFn .= "}".PHP_EOL;
-            $params['change'] = $sliderUpdateFnName;
-
-            $this->jquery->addJavascript($sliderUpdateFn);
+            $params['change'] = new Zend_Json_Expr($sliderUpdateFn);
         }
 
         $attribs['id'] .= "-slider";
 
         if(count($params) > 0) {
             $params = ZendX_JQuery::encodeJson($params);
-            $params = str_replace('"'.$sliderUpdateFnName.'"', $sliderUpdateFnName, $params);
         } else {
             $params = '{}';
         }
@@ -111,5 +97,68 @@ class ZendX_JQuery_View_Helper_Slider extends ZendX_JQuery_View_Helper_UiWidget
         $html .= '</div>';
 
         return $hidden.$html;
+    }
+
+    protected function getChangeCallback($jqh, $sliderHiddenId, $elementId, $handlerNum)
+    {
+        if(version_compare($this->jquery->getUiVersion(), "1.7.0") >= 0) {
+            return sprintf('    %s("#%s").attr("value", %s("#%s-slider").slider("values", %d));'.PHP_EOL,
+                $jqh, $sliderHiddenId, $jqh, $elementId, $handlerNum
+            );
+        } else {
+            return sprintf('    %s("#%s").attr("value", %s("#%s-slider").slider("value", %d));'.PHP_EOL,
+                $jqh, $sliderHiddenId, $jqh, $elementId, $handlerNum
+            );
+        }
+    }
+
+    protected function getHandleCount($params)
+    {
+        if(version_compare($this->jquery->getUiVersion(), "1.7.0") >= 0) {
+            return count($params['values']);
+        } else {
+            return count($params['handles']);
+        }
+    }
+
+    protected function getHandleValue($handleNum, $params)
+    {
+        if(version_compare($this->jquery->getUiVersion(), "1.7.0") >= 0) {
+            return $params['values'][$handleNum];
+        } else {
+            return $params['handles'][$handleNum]['start'];
+        }
+    }
+
+    protected function initializeStartingValues($value, $params)
+    {
+        $values = array();
+        if(isset($params['value'])) {
+            $values[] = $params['value'];
+            unset($params['value']);
+        } else if(isset($params['values'])) {
+            $values = $params['values'];
+            unset($params['values']);
+        } else if(isset($params['handles'])) {
+            for($i = 0; $i < count($params['handles']); $i++) {
+                $values[] = $params['handles'][$i]['start'];
+            }
+            unset($params['handles']);
+        } else if(isset($params['startValue'])) {
+            $values[] = $params['startValue'];
+            unset($params['startValue']);
+        } else if(is_numeric($value)) {
+            $values[] = $value;
+        }
+
+        if(version_compare($this->jquery->getUiVersion(), "1.7.0") >= 0) {
+            $params['values'] = $values;
+        } else {
+            $params['handles'] = array();
+            for($i = 0; $i < count($values); $i++) {
+                $params['handles'][$i]['start'] = $values[$i];
+            }
+        }
+        return $params;
     }
 }
--- tests/ZendX/JQuery/View/SliderTest.php
+++ tests/ZendX/JQuery/View/SliderTest.php
@@ -89,13 +89,37 @@ class ZendX_JQuery_View_SliderTest extends PHPUnit_Framework_TestCase
         $this->assertContains('"option":"true"', $jquery);
     }
 
-    public function testShouldBuiltSliderElements()
+    public function testShouldBuiltSliderElementsWith15API()
     {
+        $this->view->jQuery()->setUiVersion("1.5.2");
         $element = $this->view->slider("elem1", "75", array(), array());
 
         $this->assertEquals(
             array(
-           		'$("#elem1-slider").slider({"startValue":"75","change":zfjSliderUpdateelem1});'
+           		'$("#elem1-slider").slider({"handles":[{"start":"75"}],"change":function(e, ui) {
+    $("#elem1").attr("value", $("#elem1-slider").slider("value", 0));
+}
+});'
+            ), $this->jquery->getOnLoadActions());
+        $this->assertContains("<div", $element);
+        $this->assertContains('<div class="ui-slider-handle"></div>', $element);
+        $this->assertContains('<input', $element);
+        $this->assertContains('type="hidden"', $element);
+        $this->assertContains('id="elem1"', $element);
+        $this->assertContains('id="elem1-slider"', $element);
+    }
+
+    public function testShouldBuiltSliderElementsWith17API()
+    {
+        $this->view->jQuery()->setUiVersion("1.7.2");
+        $element = $this->view->slider("elem1", "75", array(), array());
+
+        $this->assertEquals(
+            array(
+           		'$("#elem1-slider").slider({"values":["75"],"change":function(e, ui) {
+    $("#elem1").attr("value", $("#elem1-slider").slider("values", 0));
+}
+});'
             ), $this->jquery->getOnLoadActions());
         $this->assertContains("<div", $element);
         $this->assertContains('<div class="ui-slider-handle"></div>', $element);
