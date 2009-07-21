cebbf9c3868b519f7e1c7016dbed1b8b3129f9e3
beberlei <beberlei@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-07-21 19:43:18 +0000

    ZF-7032 - Mix of Single and Double Brackets makes _htmlAttribs() helper function go nuts.
    git-svn-id: http://framework.zend.com/svn/framework/extras/trunk@16931 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/ZendX/JQuery/View/AjaxLinkTest.php
+++ tests/ZendX/JQuery/View/AjaxLinkTest.php
@@ -31,177 +31,169 @@ require_once "Zend/View.php";
 require_once "ZendX/JQuery.php";
 require_once "ZendX/JQuery/View/Helper/JQuery.php";
 
-class ZendX_JQuery_View_AjaxLinkTest extends PHPUnit_Framework_TestCase
-{
-	private $view = null;
-	private $helper = null;
+class ZendX_JQuery_View_AjaxLinkTest extends PHPUnit_Framework_TestCase {
+    private $view = null;
+    private $helper = null;
 
-	public function setUp()
-	{
+    public function setUp() {
         Zend_Registry::_unsetInstance();
         $this->view   = $this->getView();
         $this->helper = new ZendX_JQuery_View_Helper_JQuery_Container();
         $this->helper->setView($this->view);
         Zend_Registry::set('ZendX_JQuery_View_Helper_JQuery', $this->helper);
-	}
+    }
 
-	public function tearDown()
-	{
-		ZendX_JQuery_View_Helper_JQuery::disableNoConflictMode();
-	}
+    public function tearDown() {
+        ZendX_JQuery_View_Helper_JQuery::disableNoConflictMode();
+    }
 
-    public function getView()
-    {
+    public function getView() {
         require_once 'Zend/View.php';
         $view = new Zend_View();
         $view->addHelperPath('ZendX/JQuery/View/Helper/', 'ZendX_JQuery_View_Helper');
         return $view;
     }
 
-	public function testShouldBeCallable()
-	{
-		$link = $this->view->ajaxLink("Link to Inject", "inject.html");
-		$this->assertContains('Link to Inject', $link);
-		$this->assertContains('class="ajaxLink', $link);
-		$this->assertNotContains('inject.html', $link);
-		$this->assertNotContains('$.get', $link);
+    public function testShouldBeCallable() {
+        $link = $this->view->ajaxLink("Link to Inject", "inject.html");
+        $this->assertContains('Link to Inject', $link);
+        $this->assertContains('class="ajaxLink', $link);
+        $this->assertNotContains('inject.html', $link);
+        $this->assertNotContains('$.get', $link);
 
-		$render = $this->helper->__toString();
-		$this->assertContains('inject.html', $render);
-		$this->assertContains('$.get', $render);
-	}
+        $render = $this->helper->__toString();
+        $this->assertContains('inject.html', $render);
+        $this->assertContains('$.get', $render);
+    }
 
-	public function testShouldBeCallableInline()
-	{
-		$link = $this->view->ajaxLink("Link to Inject", "inject.html", array('inline' => true));
-		$this->assertContains('Link to Inject', $link);
-		$this->assertNotContains('class="ajaxLink', $link);
-		$this->assertContains('inject.html', $link);
-		$this->assertContains('$.get', $link);
+    public function testShouldBeCallableInline() {
+        $link = $this->view->ajaxLink("Link to Inject", "inject.html", array('inline' => true));
+        $this->assertContains('Link to Inject', $link);
+        $this->assertNotContains('class="ajaxLink', $link);
+        $this->assertContains('inject.html', $link);
+        $this->assertContains('$.get', $link);
 
-		$render = $this->helper->__toString();
-		$this->assertNotContains('inject.html', $render);
-		$this->assertNotContains('$.get', $render);
-	}
+        $render = $this->helper->__toString();
+        $this->assertNotContains('inject.html', $render);
+        $this->assertNotContains('$.get', $render);
+    }
 
-	public function testShouldAllowSendingParamsWithPost()
-	{
-		$link = $this->view->ajaxLink("Link to Inject2", "inject.php", array('update' => '#test', 'class' => 'someClass'), array('key' => 'value'));
-		$this->assertContains('Link to Inject2', $link);
-		$this->assertContains('class="someClass ajaxLink', $link);
-		$this->assertNotContains('inject.php', $link);
+    public function testShouldAllowSendingParamsWithPost() {
+        $link = $this->view->ajaxLink("Link to Inject2", "inject.php", array('update' => '#test', 'class' => 'someClass'), array('key' => 'value'));
+        $this->assertContains('Link to Inject2', $link);
+        $this->assertContains('class="someClass ajaxLink', $link);
+        $this->assertNotContains('inject.php', $link);
 
-		$render = $this->helper->__toString();
-		$this->assertContains('inject.php', $render);
-		$this->assertContains('$.post', $render);
-		$this->assertContains('#test', $render);
-		$this->assertContains('{"key":"value"}', $render);
-	}
+        $render = $this->helper->__toString();
+        $this->assertContains('inject.php', $render);
+        $this->assertContains('$.post', $render);
+        $this->assertContains('#test', $render);
+        $this->assertContains('{"key":"value"}', $render);
+    }
 
-	public function testShouldAllowSendingParamsWithGet()
-	{
-		$link = $this->view->ajaxLink("Link to Inject3", "inject123.php", array('method' => 'get', 'update' => '#test'), array('key' => 'value'));
-		$this->assertContains('Link to Inject3', $link);
-		$this->assertContains('class="ajaxLink', $link);
-		$this->assertNotContains('inject123.php', $link);
+    public function testShouldAllowSendingParamsWithGet() {
+        $link = $this->view->ajaxLink("Link to Inject3", "inject123.php", array('method' => 'get', 'update' => '#test'), array('key' => 'value'));
+        $this->assertContains('Link to Inject3', $link);
+        $this->assertContains('class="ajaxLink', $link);
+        $this->assertNotContains('inject123.php', $link);
 
-		$render = $this->helper->__toString();
-		$this->assertContains('inject123.php', $render);
-		$this->assertContains('$.get', $render);
-		$this->assertContains('#test', $render);
-		$this->assertContains('{"key":"value"}', $render);
-	}
+        $render = $this->helper->__toString();
+        $this->assertContains('inject123.php', $render);
+        $this->assertContains('$.get', $render);
+        $this->assertContains('#test', $render);
+        $this->assertContains('{"key":"value"}', $render);
+    }
 
-	public function testShouldAllowSpecifyingDataType()
-	{
-		$link = $this->view->ajaxLink("JSON Response with Callback", "inject.php", array('complete' => 'jsonCallback(data);', 'dataType' => 'json'), array('name' => 'Ludwig von Mises', 'email' => 'mises@vienna.at'));
-		$this->assertContains('JSON Response with Callback', $link);
-		$this->assertContains('class="ajaxLink', $link);
-		$this->assertNotContains('inject.php', $link);
-		$this->assertNotContains('{"name":"Ludwig von Mises","email":"mises@vienna.at"}', $link);
+    public function testShouldAllowSpecifyingDataType() {
+        $link = $this->view->ajaxLink("JSON Response with Callback", "inject.php", array('complete' => 'jsonCallback(data);', 'dataType' => 'json'), array('name' => 'Ludwig von Mises', 'email' => 'mises@vienna.at'));
+        $this->assertContains('JSON Response with Callback', $link);
+        $this->assertContains('class="ajaxLink', $link);
+        $this->assertNotContains('inject.php', $link);
+        $this->assertNotContains('{"name":"Ludwig von Mises","email":"mises@vienna.at"}', $link);
 
-		$render = $this->helper->__toString();
-		$this->assertContains('inject.php', $render);
-		$this->assertContains('function(data, textStatus) { jsonCallback(data); }', $render);
-		$this->assertContains("'json');", $render);
-		$this->assertContains('{"name":"Ludwig von Mises","email":"mises@vienna.at"}', $render);
-	}
+        $render = $this->helper->__toString();
+        $this->assertContains('inject.php', $render);
+        $this->assertContains('function(data, textStatus) { jsonCallback(data); }', $render);
+        $this->assertContains("'json');", $render);
+        $this->assertContains('{"name":"Ludwig von Mises","email":"mises@vienna.at"}', $render);
+    }
 
-	public function testShouldWorkInNoConflictMode()
-	{
-		ZendX_JQuery_View_Helper_JQuery::enableNoConflictMode();
-		$link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'beforeSend' => 'hide'));
+    public function testShouldWorkInNoConflictMode() {
+        ZendX_JQuery_View_Helper_JQuery::enableNoConflictMode();
+        $link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'beforeSend' => 'hide'));
 
-		$this->assertContains('$j.get', $link);
-		$this->assertContains("\$j('#test')", $link);
-		$this->assertContains('$j(this).hide', $link);
-	}
+        $this->assertContains('$j.get', $link);
+        $this->assertContains("\$j('#test')", $link);
+        $this->assertContains('$j(this).hide', $link);
+    }
 
-	public function testShouldAllowSwitchUpdateDataFunc()
-	{
-		$link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'dataType' => 'text'));
-		$this->assertContains("('#test').text(data);", $link);
-	}
+    public function testShouldAllowSwitchUpdateDataFunc() {
+        $link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'dataType' => 'text'));
+        $this->assertContains("('#test').text(data);", $link);
+    }
 
-	public function testShouldAllowUsingBeforeSendEffects()
-	{
-		$effects = array(
-			'hide'	=> 'hide();',
-			'hideslow' => 'hide("slow");',
-			'hidefast' => 'hide("fast");',
-			'fadeout' => 'fadeOut();',
-			'fadeoutslow' => 'fadeOut("slow");',
-			'fadeoutfast' => 'fadeOut("fast");',
-			'slideup' => 'slideUp(1000);',
-		);
+    static public function dataBeforeSendEffects()
+    {
+        return array(
+            array('hide', "hide();"),
+            array('hideslow', "hide('slow');"),
+            array('hidefast', "hide('fast');"),
+            array('fadeout', "fadeOut();"),
+            array('fadeoutslow', "fadeOut('slow');"),
+            array('fadeoutfast', "fadeOut('fast');"),
+            array('slideup', "slideUp(1000);"),
+        );
+    }
 
-		foreach($effects AS $effect => $js) {
-			$link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'beforeSend' => $effect));
+    /**
+     * @dataProvider dataBeforeSendEffects
+     * @param string $effect
+     * @param string $js
+     */
+    public function testShouldAllowUsingBeforeSendEffects($effect, $js)
+    {
+        $link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'beforeSend' => $effect));
+        $this->assertContains(sprintf('$(this).%s', $js), $link);
 
-			$this->assertContains(sprintf('$(this).%s', $js), $link);
-		}
+        ZendX_JQuery_View_Helper_JQuery::enableNoConflictMode();
 
-		ZendX_JQuery_View_Helper_JQuery::enableNoConflictMode();
+        $link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'beforeSend' => $effect));
+        $this->assertContains(sprintf('$j(this).%s', $js), $link);
+    }
 
-		foreach($effects AS $effect => $js) {
-			$link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'beforeSend' => $effect));
+    static public function dataCompleteEffects()
+    {
+        return array(
+            array('show', 'show();'),
+            array('showslow', "show('slow');"),
+            array('showfast', "show('fast');"),
+            array('shownormal', "show('normal');"),
+            array('fadein', "fadeIn('normal');"),
+            array('fadeinslow', "fadeIn('slow');"),
+            array('fadeinfast', "fadeIn('fast');"),
+            array('slidedown', "slideDown('normal');"),
+            array('slidedownslow', "slideDown('slow');"),
+            array('slidedownfast', "slideDown('fast');"),
+        );
+    }
 
-			$this->assertContains(sprintf('$j(this).%s', $js), $link);
-		}
-	}
+    /**
+     * @dataProvider dataCompleteEffects
+     * @param <type> $effect
+     * @param <type> $js
+     */
+    public function testShouldAllowUsingCompleteEffects($effect, $js)
+    {
+        $link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'complete' => $effect));
+        $this->assertContains(sprintf("$('#test').%s", $js), $link);
 
-	public function testShouldAllowUsingCompleteEffects()
-	{
-		$effects = array(
-			'show' => 'show();',
-			'showslow' => "show('slow');",
-			'showfast' => "show('fast');",
-			'shownormal' => "show('normal');",
-			'fadein' => "fadeIn('normal');",
-			'fadeinslow' => "fadeIn('slow');",
-			'fadeinfast' => "fadeIn('fast');",
-			'slidedown' => "slideDown('normal');",
-			'slidedownslow' => "slideDown('slow');",
-			'slidedownfast' => "slideDown('fast');",
-		);
+        ZendX_JQuery_View_Helper_JQuery::enableNoConflictMode();
 
-		foreach($effects AS $effect => $js) {
-			$link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'complete' => $effect));
+        $link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'complete' => $effect));
+        $this->assertContains(sprintf("\$j('#test').%s", $js), $link);
+    }
 
-			$this->assertContains(sprintf("$('#test').%s", $js), $link);
-		}
-
-		ZendX_JQuery_View_Helper_JQuery::enableNoConflictMode();
-
-		foreach($effects AS $effect => $js) {
-			$link = $this->view->ajaxLink("Link to Inject", "inject.html", array('update' => '#test', 'inline' => true, 'complete' => $effect));
-
-			$this->assertContains(sprintf("\$j('#test').%s", $js), $link);
-		}
-	}
-
-	public function testOptionsArrayAllowsForSettingAttributes()
-	{
+    public function testOptionsArrayAllowsForSettingAttributes() {
         $view = $this->getView();
 
         $html = $view->ajaxLink("Label1", "/some/url", array(
@@ -217,10 +209,9 @@ class ZendX_JQuery_View_AjaxLinkTest extends PHPUnit_Framework_TestCase
         $this->assertNotContains('href="#"', $html);
         $this->assertContains('class="test"', $html);
         $this->assertContains('target="_blank"', $html);
-	}
+    }
 
-	public function testSpecifyingIdDoesNotCreateAutomaticCallbackAndClassAttribute()
-	{
+    public function testSpecifyingIdDoesNotCreateAutomaticCallbackAndClassAttribute() {
         $view = $this->getView();
 
         $html = $view->ajaxLink("Label1", "/some/url", array(
@@ -229,13 +220,12 @@ class ZendX_JQuery_View_AjaxLinkTest extends PHPUnit_Framework_TestCase
 
         $this->assertNotContains('class=', $html);
         $this->assertContains('id="someId"', $html);
-	}
+    }
 
     /**
      * @group ZF-5041
      */
-    public function testXhtmlDoctypeDoesNotMakeAnchorInvalidHtml()
-    {
+    public function testXhtmlDoctypeDoesNotMakeAnchorInvalidHtml() {
         $view = $this->getView();
         $view->doctype('XHTML1_STRICT');
 
