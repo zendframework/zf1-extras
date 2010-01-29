1f6db50042ee96cb411735680d2d04c9e501c7af
beberlei <beberlei@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-01-29 09:55:19 +0000

    [GENERIC] Fixed formatting of code
    git-svn-id: http://framework.zend.com/svn/framework/extras/trunk@20744 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/ZendX/JQuery/View/Helper/DialogContainer.php
+++ library/ZendX/JQuery/View/Helper/DialogContainer.php
@@ -45,8 +45,8 @@ class ZendX_JQuery_View_Helper_DialogContainer extends ZendX_JQuery_View_Helper_
      * @param  array $attribs
      * @return string
      */
-	public function dialogContainer($id, $content, $params=array(), $attribs=array())
-	{
+    public function dialogContainer($id, $content, $params=array(), $attribs=array())
+    {
         if (!array_key_exists('id', $attribs)) {
             $attribs['id'] = $id;
         }
@@ -58,17 +58,17 @@ class ZendX_JQuery_View_Helper_DialogContainer extends ZendX_JQuery_View_Helper_
         }
 
         $js = sprintf('%s("#%s").dialog(%s);',
-            ZendX_JQuery_View_Helper_JQuery::getJQueryHandler(),
-            $attribs['id'],
-            $params
+                ZendX_JQuery_View_Helper_JQuery::getJQueryHandler(),
+                $attribs['id'],
+                $params
         );
         $this->jquery->addOnLoad($js);
 
         $html = '<div'
-              . $this->_htmlAttribs($attribs)
-              . '>'
-              . $content
-              . '</div>';
+                . $this->_htmlAttribs($attribs)
+                . '>'
+                . $content
+                . '</div>';
         return $html;
-	}
+    }
 }
