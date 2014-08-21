74393b49ef2577e6f822ac8a180ed4cda3897f1c
Frank Brückner <dev@froschdesignstudio.de>
2014-08-21 17:55:23 +0200

    Fixes ZendX_Db_AllTests

--- tests/ZendX/Db/AllTests.php
+++ tests/ZendX/Db/AllTests.php
@@ -21,26 +21,14 @@
  * @version    $Id: $
  */
 
-
-if (!defined('PHPUnit_MAIN_METHOD')) {
-    define('PHPUnit_MAIN_METHOD', 'ZendX_Db_AllTests::main');
-}
-
-
 /**
  * Test helper
  */
 require_once dirname(__FILE__) . '/../../TestHelper.php';
 
-/**
- * PHPUnit_Framework_TestSuite
- */
-require_once 'PHPUnit/Framework/TestSuite.php';
-
-/**
- * PHPUnit_TextUI_TestRunner
- */
-require_once 'PHPUnit/TextUI/TestRunner.php';
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'ZendX_Db_AllTests::main');
+}
 
 /**
  * @see Zend_Loader
